e0fd59b7924e8716968b3492386f421af397742e
Furret-png <69658166+Furret-png@users.noreply.github.com>
2022-04-26 15:59:39 -0400

    gets rid of many useless forevers

--- main.ts
+++ main.ts
@@ -6,17 +6,17 @@ input.onButtonPressed(Button.A, function () {
             charPos = 0
         }
         led.unplot(charPos + 1, 4)
-    } 
+    }
     /// start
-    else if (start==0) {
+    else if (start == 0) {
         lvlSelect = lvlSelect + 1
         if (lvlSelect > 3) {
             lvlSelect = 1
-      }
+        }
     }
 })
 input.onButtonPressed(Button.B, function () {
-    if (gameOver == 1 && start==1) {
+    if (gameOver == 1 && start == 1) {
         charPos += 1
         if (charPos > 4) {
             charPos = 4
@@ -26,24 +26,27 @@ input.onButtonPressed(Button.B, function () {
 })
 /// choose level
 input.onButtonPressed(Button.AB, function () {
-    if(start==0) {
+    if (start == 0) {
         basic.clearScreen()
         ramping = 2000 / challengeLvl
         start = 1
-        }
-})
-/// helps define ramping, shows challenge level at start, flashing lights at game over, and score showing
-basic.forever(function () {
-    if (lvlSelect==1) {
-        challengeLvl=1.9
-    } else if(lvlSelect==2) {
-        challengeLvl=2
-    } else if(lvlSelect==3) {
-        challengeLvl=2.1
     }
+})
+/// everything that needs to be in a forever loop without pauses
+basic.forever(function () {
+    /// defines speed of falling object based on selected difficulty
+    if (lvlSelect == 1) {
+        challengeLvl = 1.96
+    } else if (lvlSelect == 2) {
+        challengeLvl = 2.04
+    } else if (lvlSelect == 3) {
+        challengeLvl = 2.14
+    }
+    /// shows the selected challenge level before playing
     if (start == 0) {
         basic.showNumber(lvlSelect)
     }
+    /// flashing lights on game over
     if (life < 1 && bright >= 0) {
         gameOver = 0
         led.setBrightness(bright)
@@ -51,6 +54,7 @@ basic.forever(function () {
         led.toggleAll()
         pause(100)
     }
+    /// shows score after flashing lights
     if (life < 1 && bright <= 0 && thisIsScore == 1) {
         pause(20)
         basic.clearScreen()
@@ -60,6 +64,10 @@ basic.forever(function () {
         thisIsScore = 0
         basic.clearScreen()
         restart = 0
+    }
+    /// draws character
+    if (gameOver == 1 && start == 1) {
+        led.plot(charPos, 4)
     }
 })
 /// defining of variables and setting of brightness
@@ -77,16 +85,10 @@ let thisIsScore = 1
 let gameOver = 1
 let restart = 1
 led.setBrightness(bright)
-/// score showing
-/// drawing of character
-basic.forever(function () {
-    if (gameOver == 1 &&start==1) {
-        led.plot(charPos, 4)
-    }
-})
+
 /// drawing of coin, score giving/life taking, and difficulty ramping
 basic.forever(function () {
-    if (life > 0 && start ==1) {
+    if (life > 0 && start == 1) {
         basic.pause(ramping)
         sprPosX = randint(0, 4)
         sprPosY = 0
@@ -117,7 +119,7 @@ basic.forever(function () {
 input.onGesture(Gesture.Shake, function () {
     if (restart == 0) {
         score = 0
-        gameOver=1
+        gameOver = 1
         charPos = 2
         life = 3
         ramping = 1000
@@ -125,6 +127,6 @@ input.onGesture(Gesture.Shake, function () {
         thisIsScore = 1
         basic.clearScreen()
         restart = 1
-        start =0
+        start = 0
     }
 })
